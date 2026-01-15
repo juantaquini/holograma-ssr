5fb1e83ce4c5263de0d74fa8d5e027eb1292edf3
juantaquini <taquinibautista@gmail.com>
2026-01-15 10:23:21 -0300

    fix: use supported next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "/Users/bati/holograma",
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -15,4 +11,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
